11be44859f2bc1f055abb5124cd8901cad164ab5
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-10 00:08:30 -0700

    digital clock tutorial working

--- ClockViewController.swift
+++ ClockViewController.swift
@@ -21,5 +21,19 @@ class ClockViewController: UIViewController {
      
     }
 
-
+  func updateClock() {
+    
+    let date = NSDate()
+    let calendar = NSCalendar.currentCalendar()
+    let components = calendar.components([.Hour, .Minute, .Second], fromDate: date)
+    
+    var hour = components.hour > 12 ? components.hour - 12 : components.hour
+    hour = hour == 0 ? 12 : hour
+    let hourString = hour > 9 ? "\(hour)" : "0\(hour)"
+    let minutes = components.minute > 9 ? "\(components.minute)" : "0\(components.minute)"
+    let seconds = components.second > 9 ? "\(components.second)" : "0\(components.second)"
+    let am = components.hour > 12 ? "AM" : "PM"
+    
+    myTimeLabel.text = "\(hourString):\(minutes):\(seconds) \(am)"
+  }
 }
