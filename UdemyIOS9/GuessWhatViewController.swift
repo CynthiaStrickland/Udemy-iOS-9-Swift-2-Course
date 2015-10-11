4f5512e8d19347440e67c785f8146bc4fb9bd414
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-10 17:29:42 -0700

    Guess Who Game add code

--- UdemyIOS9/GuessWhatViewController.swift
+++ UdemyIOS9/GuessWhatViewController.swift
@@ -10,26 +10,69 @@ import UIKit
 
 class GuessWhatViewController: UIViewController {
 
+  @IBOutlet weak var questionLabel: UILabel!
+  
+  @IBOutlet weak var answerTextField: UITextField!
+  
+  var question : Int = 2
+  
+  @IBAction func nextButtonPressed(sender: AnyObject) {
+    
+    if question == 1 {
+    
+      questionLabel.text = "Think of a number"
+      answerTextField.text = ""
+      answerTextField.hidden = true
+      sender.setTitle("Next", forState: UIControlState.Normal)
+      
+    } else if question == 2 {
+      questionLabel.text = "Multiply it by 5"
+    } else if question == 3 {
+      questionLabel.text = "Now add 5 to the total"
+    } else if question == 4 {
+      questionLabel.text = "Multiply the answer by 2"
+    } else if question == 5 {
+      questionLabel.text = "Now add 2 to the total"
+    } else if question == 6 {
+      questionLabel.text = "What is the total"
+      answerTextField.hidden = false
+    } else if question == 7 {
+      if answerTextField.text != "" {
+        let input : Int = Int(answerTextField.text!)!
+        let answer = ((((input - 2) / 2) - 5) / 5)
+        questionLabel.text = "You thought of : \(answer)"
+      }
+    }
+}
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+}
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+      questionLabel.text = ""
+      answerTextField.text = ""
+      answerTextField.hidden = true
+      
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
+  override func preferredStatusBarStyle() -> UIStatusBarStyle {
+    return UIStatusBarStyle.LightContent
+  }
 
 }
