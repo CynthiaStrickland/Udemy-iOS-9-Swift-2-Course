fdd2457fdf0ba9e4eb6fbf4d93f1308ed93ec5f0
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-11 22:53:26 -0700

    Web Browser App Interface Set=up

---
+++ UdemyIOS9/WebBrowserViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebBrowserViewController.swift
+//  UdemyIOS9
+//
+//  Created by Cynthia Whitlatch on 10/11/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+class WebBrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
