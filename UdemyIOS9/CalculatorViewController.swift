590ea74ef22872748339f58ec33fc918fca538f2
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-11 19:54:59 -0700

    Calculator stackView Constraints added

--- UdemyIOS9/CalculatorViewController.swift
+++ UdemyIOS9/CalculatorViewController.swift
@@ -10,26 +10,26 @@ import UIKit
 
 class CalculatorViewController: UIViewController {
 
+  @IBOutlet weak var Label: UILabel!
+  
+  @IBAction func buttonOperation(sender: AnyObject) {
+  }
+  
+  @IBAction func buttonDigitPressed(sender: AnyObject) {
+  }
+  
+  @IBAction func cancelButton(sender: AnyObject) {
+  }
+  
+  
+  
+  
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
