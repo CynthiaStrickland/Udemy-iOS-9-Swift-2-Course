53c90f2f933e0e7caded48d7301022bdf222919f
Cynthia Whitlatch <cawhitlatch2@gmail.com>
2015-10-16 13:03:45 -0700

    Core Data Proj - Interface Set-up for SearchViewController and AutoLayout

---
+++ UdemyIOS9COREDATA/UdemyIOS9COREDATA/NewContactViewController.swift
@@ -0,0 +1,44 @@
+//
+//  NewContactViewController.swift
+//  UdemyIOS9COREDATA
+//
+//  Created by Cynthia Whitlatch on 10/16/15.
+//  Copyright © 2015 Cynthia Whitlatch. All rights reserved.
+//
+
+import UIKit
+
+var firstName = String?()
+var lastName = String?()
+var phoneNumber = String?()
+var email = String?()
+
+class NewContactViewController: UIViewController, UITextFieldDelegate {
+
+  
+  
+  
+  @IBOutlet weak var firstNameTextField: UITextField!
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var phoneTextField: UITextField!
+  @IBOutlet weak var lastNameTextField: UITextField!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
